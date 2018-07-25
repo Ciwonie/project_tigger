9b2f2cb498f6d35ad781fd702fdba19ef3cdeb3b
Ciwonie <ciwonie@gmail.com>
2018-07-25 01:18:20 -0500

    almost done with index page, added pulsing button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,21 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $('.sidenav').sidenav();
 
-    $(document).on('click', '.index-button', function() {
+    $(document).on('click', '.index-button', function () {
         playSound();
         console.log('sound played');
+        redirect()
     });
+
+    function redirect() {
+        setTimeout(function () {
+            location.href = "gallery.html"; //will redirect to gallery
+            console.log('redirect');
+        }, 1500); //will call the function after 2 secs.
+    }
 
     function playSound() {
         var sound = document.getElementById("audio");
         sound.play();
     }
-  });
+});
